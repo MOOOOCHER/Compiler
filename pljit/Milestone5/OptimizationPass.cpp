914a3b220c372078a9cb375342f1971a5cc190f5
Daniel Chen <daniel.chen@tum.de>
2022-08-09 11:04:01 +0200

    - Milestone 5: some refactoring

--- pljit/Milestone5/OptimizationPass.cpp
+++ pljit/Milestone5/OptimizationPass.cpp
@@ -2,8 +2,8 @@
 namespace semantic{
     void DeadCodeEliminationPass::optimize(ASTNode& node) {
         if(node.getType() == ASTNode::FunctionDefinition){
-            auto functionDefinition = static_cast<ASTFunctionNode*>(&node);
-            for(auto& child: functionDefinition->children){
+            auto& functionDefinition = static_cast<ASTFunctionNode&>(node);
+            for(auto& child: functionDefinition.children){
                 if(child->getType() == ASTNode::CompoundStatement){
                     optimize(*child);
                     return;
@@ -11,21 +11,21 @@ namespace semantic{
             }
         }
         else if(node.getType() == ASTNode::CompoundStatement){
-            auto compoundStatement = static_cast<ASTCompoundStatement*>(&node);
+            auto& compoundStatement = static_cast<ASTCompoundStatement&>(node);
             while(true){
                 //start removing statements from the end until return statement is reached
-                if(compoundStatement->children[compoundStatement->children.size()-1]->getType() == ASTNode::ReturnStatement){
+                if(compoundStatement.children[compoundStatement.children.size()-1]->getType() == ASTNode::ReturnStatement){
                     return;
                 }
-                compoundStatement->pop_back_child();
+                compoundStatement.pop_back_child();
             }
         }
     }
     //ConstantPropagation---------------------------------------------------------------------------------------------------------
     void ConstantPropagationPass::optimize(ASTNode& node) {
         if(node.getType() == ASTNode::FunctionDefinition){
-            auto functionDefinition = static_cast<ASTFunctionNode*>(&node);
-            for(auto& child: functionDefinition->children){
+            auto& functionDefinition = static_cast<ASTFunctionNode&>(node);
+            for(auto& child: functionDefinition.children){
                 switch(child->getType()){
                     case ASTNode::InitDeclaratorList: {
                         optimize(*child);
@@ -40,8 +40,8 @@ namespace semantic{
             }
         } else if(node.getType() == ASTNode::InitDeclaratorList){
             //initialize table with constant values
-            auto constInitDeclList = static_cast<ASTInitDeclaratorListNode*>(&node);
-            for(auto& child: constInitDeclList->children){
+            auto& constInitDeclList = static_cast<ASTInitDeclaratorListNode&>(node);
+            for(auto& child: constInitDeclList.children){
                 auto constInitDecl = static_cast<ASTInitDeclaratorNode*>(child.get());
                 //get Values
                 auto name = static_cast<ASTIdentifierNode*>(constInitDecl->leftChild.get());
@@ -50,66 +50,66 @@ namespace semantic{
             }
         } else if(node.getType() == ASTNode::CompoundStatement){
             //optimize each statement
-            auto compoundStatement = static_cast<ASTCompoundStatement*>(&node);
-            for(auto& child: compoundStatement->children){
+            auto& compoundStatement = static_cast<ASTCompoundStatement&>(node);
+            for(auto& child: compoundStatement.children){
                     optimizeStatement(*child);
             }
         }
     }
     void ConstantPropagationPass::optimizeStatement(ASTNode& node){
         if(node.getType() == ASTNode::ReturnStatement){
-            auto statement = static_cast<ASTReturnStatementNode*>(&node);
-            auto optimized = optimizeExpression(*statement->child);
+            auto& statement = static_cast<ASTReturnStatementNode&>(node);
+            auto optimized = optimizeExpression(*statement.child);
             if(optimized.has_value()){
                 //change child to constant
-                statement->child = std::make_unique<ASTLiteralNode>(optimized.value());
+                statement.child = std::make_unique<ASTLiteralNode>(optimized.value());
             }
         } else {
-            auto statement = static_cast<ASTAssignmentStatementNode*>(&node);
-            optimizeExpression(*statement->child);
+            auto& statement = static_cast<ASTAssignmentStatementNode&>(node);
+            optimizeExpression(*statement.child);
         }
     }
     std::optional<double> ConstantPropagationPass::optimizeExpression(ASTNode& node){
         if(node.getType() == ASTNode::Constant){
-            auto constantExpr = static_cast<ASTIdentifierNode*>(&node);
-            return variables.find(constantExpr->getValue())->second;
+            auto& constantExpr = static_cast<ASTIdentifierNode&>(node);
+            return variables.find(constantExpr.getValue())->second;
         } else if(node.getType() == ASTNode::LiteralConstant){
-            auto literalExpr = static_cast<ASTLiteralNode*>(&node);
-            return literalExpr->getValue();
+            auto& literalExpr = static_cast<ASTLiteralNode&>(node);
+            return literalExpr.getValue();
         }else if(node.getType() == ASTNode::Variable){
-            auto constantExpr = static_cast<ASTIdentifierNode*>(&node);
-            auto variableName = constantExpr->getValue();
+            auto& constantExpr = static_cast<ASTIdentifierNode&>(node);
+            auto variableName = constantExpr.getValue();
             if(variables.contains(variableName)){
                 return variables.find(variableName)->second;
             }
         } else if(node.getType() == ASTNode::AssignmentExpression){
-            auto assignmentExpr = static_cast<ASTAssignmentExpression*>(&node);
-            auto optimized = optimizeExpression(*assignmentExpr->rightChild);
+            auto& assignmentExpr = static_cast<ASTAssignmentExpression&>(node);
+            auto optimized = optimizeExpression(*assignmentExpr.rightChild);
             if(optimized.has_value()){
-                auto name = static_cast<ASTIdentifierNode*>(assignmentExpr->leftChild.get());
+                auto name = static_cast<ASTIdentifierNode*>(assignmentExpr.leftChild.get());
                 //change child to constant
-                assignmentExpr->rightChild = std::make_unique<ASTLiteralNode>(optimized.value());
+                assignmentExpr.rightChild = std::make_unique<ASTLiteralNode>(optimized.value());
                 variables.insert(std::make_pair(name->getValue(),optimized.value()));
             }
             return {};
         }
         else if(node.getType() == ASTNode::UnaryPlus || node.getType() == ASTNode::UnaryMinus){
-            auto unaryExpr = static_cast<ASTUnaryExpression*>(&node);
-            auto optimized = optimizeExpression(*unaryExpr->child);
+            auto& unaryExpr = static_cast<ASTUnaryExpression&>(node);
+            auto optimized = optimizeExpression(*unaryExpr.child);
             if(optimized.has_value()){
                 //change child to constant
                 if(node.getType() == ASTNode::UnaryMinus){
                     optimized = std::optional<double>(optimized.value()*-1);
                 }
-                unaryExpr->child = std::make_unique<ASTLiteralNode>(optimized.value());
+                unaryExpr.child = std::make_unique<ASTLiteralNode>(optimized.value());
             }
             return optimized;
         } else if(node.getType() == ASTNode::PlusOperator || node.getType() == ASTNode::MinusOperator
                    || node.getType() == ASTNode::MulOperator || node.getType() == ASTNode::DivOperator){
             //binary operation
-            auto expr = static_cast<ASTOperationExpressionNode*>(&node);
-            auto optimizedLeft = optimizeExpression(*expr->leftChild);
-            auto optimizedRight = optimizeExpression(*expr->rightChild);
+            auto& expr = static_cast<ASTOperationExpressionNode&>(node);
+            auto optimizedLeft = optimizeExpression(*expr.leftChild);
+            auto optimizedRight = optimizeExpression(*expr.rightChild);
             if(optimizedLeft.has_value() && optimizedRight.has_value()){
                 //change child to constant
                 if(node.getType() == ASTNode::PlusOperator){
@@ -125,11 +125,11 @@ namespace semantic{
             } else if(optimizedRight.has_value()){
               //if only right child is constant
                 double value = optimizedRight.value();
-                expr->rightChild = std::make_unique<ASTLiteralNode>(value);
+                expr.rightChild = std::make_unique<ASTLiteralNode>(value);
             } else if(optimizedLeft.has_value()){
                 //if only left child is constant
                 double value = optimizedLeft.value();
-                expr->leftChild = std::make_unique<ASTLiteralNode>(value);
+                expr.leftChild = std::make_unique<ASTLiteralNode>(value);
             }
         }
         return {};
